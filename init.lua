285b1ce6d4ef7c91f1fb4624f7061d063e8eef2e
Jesse Govindan <j.r.govindan@gmail.com>
2023-06-01 08:58:20 +0200

    Add tab preferences

--- init.lua
+++ init.lua
@@ -1,14 +1,14 @@
 -- Lazy package manager bootstrap
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+	"git",
+	"clone",
+	"--filter=blob:none",
+	"https://github.com/folke/lazy.nvim.git",
+	"--branch=stable", -- latest stable release
+	lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -47,3 +47,8 @@ require('onedark').setup {
 	style = 'darker'
 }
 require('onedark').load()
+
+-- Indentation and Tabs
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
