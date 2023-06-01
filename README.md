f0307d31bad78aef6d5179d57a0a47d23f8130c1
Jesse Govindan <j.r.govindan@gmail.com>
2023-06-01 07:39:08 +0200

    Add package manager and file explorer package

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Goals
 - Create an editing experience that will allow me to maximise productivity
+
+# Required tools
+- xclip (apt)
+
+# Require fonts
+- FiraCode Nerd Font
---
+++ init.lua
@@ -0,0 +1,39 @@
+-- Lazy package manager bootstrap
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.opt.termguicolors = true
+
+require("lazy").setup({
+	"nvim-tree/nvim-tree.lua",
+	"nvim-tree/nvim-web-devicons",
+})
+
+-- nvim-tree setup
+require("nvim-tree").setup({
+	sort_by = "case_sensitive",
+	view = {
+		width = 30,
+	},
+	renderer = {
+		group_empty = true,
+	},
+	filters = {
+		dotfiles = true,
+	},
+})
+
+vim.keymap.set('n', '<C-n>', ':NvimTreeToggle<CR>')
