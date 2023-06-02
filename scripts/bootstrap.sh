5c921965d389488a2b0f67dadbb67ec8727a21a4
Jesse Govindan <j.r.govindan@gmail.com>
2023-06-02 14:39:31 +0200

    Add kitty config

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -3,3 +3,10 @@ rm -rf ~/.tmux/plugins/tpm
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 rm -f ~/.tmux.conf
 ln -sr ./tmux/.tmux.conf ~/
+
+# Kitty config setup
+cd ~/
+mkdir -p kitty
+cd -
+rm -f ~/kitty/kitty.conf
+ln -sr ./kitty/kitty.conf ~/kitty/
